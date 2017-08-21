887737ed64c924c9b97be4e55ff488d0f1eb8675
labcabrera <lab.cabrera@gmail.com>
2017-08-21 17:28:47 +0200

    Added initial ms

---
+++ insurance-ms-asset/src/main/java/org/lab/insurance/ms/asset/AssetApplication.java
@@ -0,0 +1,13 @@
+package org.lab.insurance.ms.asset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetApplication.class, args);
+	}
+
+}
