3ef095407d0009dccebf77c05b523d3176b0ebae
labcabrera <lab.cabrera@gmail.com>
2017-08-22 17:17:42 +0200

    Removed libraries

--- insurance-core/src/main/java/org/lab/insurance/core/serialization/Serializer.java
+++ insurance-core/src/main/java/org/lab/insurance/core/serialization/Serializer.java
@@ -1,51 +1,28 @@
 package org.lab.insurance.core.serialization;
 
 import java.lang.reflect.Type;
-import java.util.Date;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
+@Deprecated
 public class Serializer {
 
-	private final static Logger LOG = LoggerFactory.getLogger(Serializer.class);
-
-	private final Gson serializer;
-	private final GsonBuilder gsonBuilder;
-
-	public Serializer() {
-		LOG.info("Initializing serializer");
-		gsonBuilder = new GsonBuilder();
-		gsonBuilder.addSerializationExclusionStrategy(new DefaultExclusionStrategy());
-		gsonBuilder.registerTypeHierarchyAdapter(Date.class,
-				new DateSerializer("yyyy-MM-dd'T'HH:mm:ss.SSSZ", "dd/MM/yyyy", "ddMMyyyy"));
-		serializer = gsonBuilder.create();
-	}
-
 	public String toJson(Object src, Type typeOfSrc) {
-		return serializer.toJson(src, typeOfSrc);
+		throw new RuntimeException("deprecated");
+		// return serializer.toJson(src, typeOfSrc);
 	}
 
 	public String toJson(Object src) {
-		return serializer.toJson(src);
+		throw new RuntimeException("deprecated");
+		// return serializer.toJson(src);
 	}
 
 	public <T> T fromJson(String json, Class<T> classOfT) {
-		return serializer.fromJson(json, classOfT);
+		throw new RuntimeException("deprecated");
+		// return serializer.fromJson(json, classOfT);
 	}
 
 	public <T> T fromJson(String json, Type type) {
-		return serializer.fromJson(json, type);
+		throw new RuntimeException("deprecated");
+		// return serializer.fromJson(json, type);
 	}
 
-	public Gson get() {
-		return serializer;
-	}
-
-	public GsonBuilder getBuilder() {
-		return gsonBuilder;
-	}
 }
