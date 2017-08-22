9f848d2128ff80794429e736394e9cf76cd3cd2d
labcabrera <lab.cabrera@gmail.com>
2017-08-22 19:48:04 +0200

    Fix eureka client

--- insurance-ms-contract-creation/src/main/java/org/lab/insurance/ms/contract/creation/ContractCreationApplication.java
+++ insurance-ms-contract-creation/src/main/java/org/lab/insurance/ms/contract/creation/ContractCreationApplication.java
@@ -2,12 +2,12 @@ package org.lab.insurance.ms.contract.creation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableSwagger2
 public class ContractCreationApplication {
 
