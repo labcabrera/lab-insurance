b7b36f7fdbfd39bd450cab96b02d3092f60a732f
labcabrera <lab.cabrera@gmail.com>
2017-08-28 18:50:48 +0200

    Renamed broker core project

--- insurance-broker-core/src/main/java/org/lab/insurance/broker/core/InsuranceBrokerCoreApp.java
+++ insurance-broker-core/src/main/java/org/lab/insurance/broker/core/InsuranceBrokerCoreApp.java
@@ -1,13 +1,13 @@
-package org.lab.insurance.ms.market;
+package org.lab.insurance.broker.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MarketApplication {
+public class InsuranceBrokerCoreApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MarketApplication.class, args);
+		SpringApplication.run(InsuranceBrokerCoreApp.class, args);
 	}
 
 }
