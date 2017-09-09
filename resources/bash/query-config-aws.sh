aa1a8c97e5772a3f140a855241e63b8ae4953e78
labcabrera <lab.cabrera@gmail.com>
2017-09-09 10:50:16 +0200

    Initial aws cloud configuration

---
+++ resources/bash/query-config-aws.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+HOST="labinsurancecloudco-env.eu-west-1.elasticbeanstalk.com"
+CONFIG_NAME=$1
+PROFILE=$2
+
+if [ -z "$1" ]; then
+    CONFIG_NAME="insurance-common"
+fi
+
+if [ -z "$2" ]; then
+    PROFILE="default"
+fi
+
+URL="http://${HOST}:${PORT}/${CONFIG_NAME}/${PROFILE}"
+
+echo ${URL}
+
+curl -s ${URL} | python -m json.tool
+
