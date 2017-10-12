97214499f235b6abd1b1aeebf35df72c437b0839
labcabrera <lab.cabrera@gmail.com>
2017-10-12 11:56:58 +0200

    Eureka-Zuul-AWS intial integration

--- insurance-zuul/src/main/java/org/lab/insurance/zuul/ZuulApp.java
+++ insurance-zuul/src/main/java/org/lab/insurance/zuul/ZuulApp.java
@@ -3,19 +3,18 @@ package org.lab.insurance.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = { RabbitAutoConfiguration.class })
 @EnableZuulProxy
-@EnableDiscoveryClient
 @EnableSwagger2
+// @EnableDiscoveryClient
 // TODO revisar ClassNotFoundException:
 // org.springframework.cloud.netflix.zuul.ZuulServerAutoConfiguration
-//@EnableAdminServer
 public class ZuulApp {
 
 	public static void main(String[] args) {
