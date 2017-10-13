fcd75fa875b89612780dd52c674db97f806883d0
labcabrera <lab.cabrera@gmail.com>
2017-10-13 19:14:39 +0200

    Initial fees module

---
+++ insurance-fee-core/src/main/java/org/lab/insurance/fee/core/FeeCoreApp.java
@@ -0,0 +1,13 @@
+package org.lab.insurance.fee.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeeCoreApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeeCoreApp.class, args);
+	}
+
+}
